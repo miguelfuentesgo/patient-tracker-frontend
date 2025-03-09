9bd60718af95b4e8241546cb89817df98c0ba62a
Miguel Fuentes <miguel_eduard98@hotmail.com>
2025-03-09 00:10:38 -0500

    add initial configuration

---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  ssr: false,
+  css: ['~/assets/css/main.scss'],
+  app: {
+    head: {
+      title: 'Valentech Patient Tracker', 
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/jpeg',
+          href: '/logo.jpeg'
+        }
+      ]
+    }
+  },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>
+        VALENTECH PATIENT TRACKER
+    </h1>
+</template>
+
+<style scoped lang="scss">
+
+    h1 {
+        justify-self: center;
+    }
+</style>
