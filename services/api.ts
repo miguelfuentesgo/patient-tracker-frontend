cec85468edb2d50d9dcd561fc21043109716fc7c
Miguel Fuentes <miguel_eduard98@hotmail.com>
2025-03-11 20:13:02 -0500

    fix url

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://34.72.144.115:8000/api',
+  baseURL: 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
